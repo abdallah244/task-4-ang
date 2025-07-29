bf5a8a6adb52bba086d206a30a04c2d14903b9cf
abdallah244 <abdallahhfares@gmail.com>
2025-07-29 18:55:24 +0300

    Initial Angular task commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+ imports: [
+  RouterOutlet,
+  FormsModule,
+  ReactiveFormsModule,
+  CommonModule 
+],
+
+
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('task4');
+
+  form = new FormGroup({
+  name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  password: new FormControl('', [Validators.required, Validators.minLength(6)])
+});
+
+
+ submittedData: any = null;
+
+onSubmit() {
+  this.submittedData = this.form.value;
+  console.log('Form Submitted (Reactive):', this.form.value);
+}
+
+
 }
